23921ed0a9007506e427ae888f572e09df174135
Uday <udaysharma4600@gmail.com>
2024-06-17 21:49:24 +0530

    17-Jun-2024

--- Easy Problems/367_Valid_Perfect_Square.cpp
+++ Easy Problems/367_Valid_Perfect_Square.cpp
@@ -1,27 +1,33 @@
 /*
 Given a positive integer num, return true if num is a perfect square or false otherwise.
-
 A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
-
 You must not use any built-in library function, such as sqrt.
-
- 
-
 Example 1:
-
 Input: num = 16
 Output: true
 Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
 Example 2:
-
 Input: num = 14
 Output: false
 Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
- 
-
 Constraints:
-
 1 <= num <= 231 - 1
+*/
+/*
+    MOST OPTIMIZED APPROACH
+Time Complexity O(1)
+Space Complexity O(1)
+*/
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        return (ceil(sqrt(num))==floor(sqrt(num)));
+    }
+};
+/*
+     OPTIMIZED APPROACH
+Time Complexity O(log(n))
+Space Complexity O(1)
 */
 class Solution {
 public:
@@ -38,6 +44,11 @@ public:
     return 0;
     }
 };
+/*
+    LEAST OPTIMIZED APPROACH
+Time Complexity O(n)
+Space Complexity O(1)
+*/
 class Solution {
 public:
     bool isPerfectSquare(int num) {
