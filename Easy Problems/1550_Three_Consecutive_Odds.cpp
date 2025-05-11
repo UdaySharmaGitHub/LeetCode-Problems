8a1da722771c09e6a5e7996600f380ef7c8a1c5a
Uday <udaysharma4600@gmail.com>
2025-05-11 16:43:40 +0530

    11-May-2025

--- Easy Problems/1550_Three_Consecutive_Odds.cpp
+++ Easy Problems/1550_Three_Consecutive_Odds.cpp
@@ -23,9 +23,7 @@ public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         int count = 0 ;
        if(arr.size()<3) return 0;
-       for(int i = 0 ;i<arr.size()-2;i++){
-        if(arr[i]&1 && arr[i+1]&1 && arr[i+2]&1) return 1;
-       }
+       for(int i = 0 ;i<arr.size()-2;i++){if(arr[i]&1 && arr[i+1]&1 && arr[i+2]&1) return 1;}
        return 0;
     }
 };
