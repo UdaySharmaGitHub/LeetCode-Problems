08570aa8dd0d6664d9e4525caa1af0785d7ea8cb
Uday <udaysharma4600@gmail.com>
2025-06-30 10:32:01 +0530

    POTD 29-June-2025

---
+++ Medium Problems/1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition.cpp
@@ -0,0 +1,51 @@
+/*
+1498. Number of Subsequences That Satisfy the Given Sum Condition
+You are given an array of integers nums and an integer target.
+Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
+Example 1:
+Input: nums = [3,5,6,7], target = 9
+Output: 4
+Explanation: There are 4 subsequences that satisfy the condition.
+[3] -> Min value + max value <= target (3 + 3 <= 9)
+[3,5] -> (3 + 5 <= 9)
+[3,5,6] -> (3 + 6 <= 9)
+[3,6] -> (3 + 6 <= 9)
+Example 2:
+Input: nums = [3,3,6,8], target = 10
+Output: 6
+Explanation: There are 6 subsequences that satisfy the condition. (nums can have repeated numbers).
+[3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
+Example 3:
+
+Input: nums = [2,3,3,4,6,7], target = 12
+Output: 61
+Explanation: There are 63 non-empty subsequences, two of them do not satisfy the condition ([6,7], [7]).
+Number of valid subsequences (63 - 2 = 61).
+
+Constraints:
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+1 <= target <= 106
+*/
+/*
+    Time Complexity: O(n*log(n))
+    Space Complexity: O(n)
+*/
+class Solution {
+public:
+const int d =1e9+7; 
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int> power(nums.size(), 1);
+        for (int i = 1; i < nums.size(); ++i) {
+            power[i] = (power[i - 1] * 2) % d;
+        }
+        int count =0 , i = 0  , j = nums.size()-1;
+        while(i<=j){
+            int sum = nums[i] + nums[j];
+            if(sum>target){ j--;}
+            else if(sum <= target){ count+=power[j-i]; count%=d; i++;}
+        }
+        return count;
+    }
+};
