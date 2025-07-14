93dc1a3d3f3923371752668bca00933f05bcd285
Uday <udaysharma4600@gmail.com>
2025-07-14 13:45:02 +0530

    14-July-2025

--- Easy Problems/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
+++ Easy Problems/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
@@ -1,24 +1,15 @@
 /*
 Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
-
 Return the decimal value of the number in the linked list.
-
 The most significant bit is at the head of the linked list.
-
- 
-
 Example 1:
 Input: head = [1,0,1]
 Output: 5
 Explanation: (101) in base 2 = (5) in base 10
 Example 2:
-
 Input: head = [0]
 Output: 0
- 
-
 Constraints:
-
 The Linked List is not empty.
 Number of nodes will not exceed 30.
 Each node's value is either 0 or 1.
@@ -48,6 +39,31 @@ public:
         }
         return i;
     }
+//  Optimized Approach Using Reversing
+//  Time Complexity O(n);
+//  Space Complexity O(1);
+class Solution {
+public:
+ListNode* reverse(ListNode* head){
+    ListNode* prev = nullptr , *nxt = nullptr, *curr = head;
+    while(curr){
+        nxt = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nxt;
+    }
+    return prev;
+}
+    int getDecimalValue(ListNode* head) {
+        head = reverse(head);
+        int res = 0 , i = 0;
+        while(head){
+            res += head->val*pow(2,i++);
+            head = head->next;
+        }
+        return res;
+    }
+};
 //  Optimized Approach 
 //  Time Complexity O(n);
 //  Space Complexity O(1);
