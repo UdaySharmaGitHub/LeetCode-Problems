c084968c9a66fa48ae9c70fa10f23da056848dc3
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-04-17 00:33:15 +0530

    POTD 16-April-2026

---
+++ Medium Problems/3488_Closest_Equal_Element_Queries.cpp
@@ -0,0 +1,66 @@
+/*
+3488. Closest Equal Element Queries
+You are given a circular array nums and an array queries.
+For each query i, you have to find the following:
+The minimum distance between the element at index queries[i] and any other index j in the circular array, where nums[j] == nums[queries[i]]. If no such index exists, the answer for that query should be -1.
+Return an array answer of the same size as queries, where answer[i] represents the result for query i.
+
+Example 1:
+Input: nums = [1,3,1,4,1,3,2], queries = [0,3,5]
+Output: [2,-1,3]
+Explanation:
+Query 0: The element at queries[0] = 0 is nums[0] = 1. The nearest index with the same value is 2, and the distance between them is 2.
+Query 1: The element at queries[1] = 3 is nums[3] = 4. No other index contains 4, so the result is -1.
+Query 2: The element at queries[2] = 5 is nums[5] = 3. The nearest index with the same value is 1, and the distance between them is 3 (following the circular path: 5 -> 6 -> 0 -> 1).
+Example 2:
+Input: nums = [1,2,3,4], queries = [0,1,2,3]
+Output: [-1,-1,-1,-1]
+Explanation:
+Each value in nums is unique, so no index shares the same value as the queried element. This results in -1 for all queries.
+
+Constraints:
+1 <= queries.length <= nums.length <= 105
+1 <= nums[i] <= 106
+0 <= queries[i] < nums.length
+*/
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        unordered_map<int, vector<int>> mp;
+
+        // store indices
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        vector<int> ans;
+
+        for (int q : queries) {
+            vector<int>& v = mp[nums[q]];
+
+            // only one occurrence
+            if (v.size() == 1) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            int pos = lower_bound(v.begin(), v.end(), q) - v.begin();
+            int res = INT_MAX;
+
+            // left neighbor
+            int left = v[(pos - 1 + v.size()) % v.size()];
+            int d1 = abs(q - left);
+            res = min(res, min(d1, n - d1));
+
+            // right neighbor
+            int right = v[(pos + 1) % v.size()];
+            int d2 = abs(q - right);
+            res = min(res, min(d2, n - d2));
+
+            ans.push_back(res);
+        }
+
+        return ans;
+    }
+};
