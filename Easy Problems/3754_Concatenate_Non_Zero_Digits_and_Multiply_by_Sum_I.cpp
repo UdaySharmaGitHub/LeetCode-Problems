58056919aacef5b91e5e2d52f9f0f6725a72fee4
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-07-18 23:30:42 +0530

    POTD 07-07-2026 : 3754 Concatenate Non Zero Digits and Multiply by Sum I [Easy]

---
+++ Easy Problems/3754_Concatenate_Non_Zero_Digits_and_Multiply_by_Sum_I.cpp
@@ -0,0 +1,44 @@
+/*
+3754. Concatenate Non-Zero Digits and Multiply by Sum I
+You are given an integer n.
+Form a new integer x by concatenating all the non-zero digits of n in their original order. If there are no non-zero digits, x = 0.
+Let sum be the sum of digits in x.
+Return an integer representing the value of x * sum.
+Example 1:
+Input: n = 10203004
+Output: 12340
+Explanation:
+The non-zero digits are 1, 2, 3, and 4. Thus, x = 1234.
+The sum of digits is sum = 1 + 2 + 3 + 4 = 10.
+Therefore, the answer is x * sum = 1234 * 10 = 12340.
+Example 2:
+Input: n = 1000
+Output: 1
+Explanation:
+The non-zero digit is 1, so x = 1 and sum = 1.
+Therefore, the answer is x * sum = 1 * 1 = 1.
+
+Constraints:
+0 <= n <= 109
+*/
+class Solution {
+public:
+long long reverse(int n ){
+    int temp = 0 ;
+    while(n){
+        temp = temp*10 + n%10;
+        n/=10;
+    }
+    return temp;
+}
+    long long sumAndMultiply(int n) {
+        int non_zero_digit_cat = 0  , sum_of_digit = 0 ;
+        while(n){
+            int digit = n%10;
+           if(digit!=0){ non_zero_digit_cat = non_zero_digit_cat*10+digit;
+            sum_of_digit += digit;}
+            n/=10; 
+        }
+        return reverse(non_zero_digit_cat)*sum_of_digit;
+    }
+};
