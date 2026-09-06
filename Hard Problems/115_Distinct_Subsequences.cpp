4f92689ce44b029b560014ea7392c0deb8240323
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-09-06 23:57:29 +0530

    POTD 06-09-2026 : 115 Distinct Subsequences [Hard]

---
+++ Hard Problems/115_Distinct_Subsequences.cpp
@@ -0,0 +1,52 @@
+/*
+115. Distinct Subsequences
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+The test cases are generated so that the answer fits on a 32-bit signed integer.
+Example 1:
+Input: s = "rabbbit", t = "rabbit"
+Output: 3
+Explanation:
+As shown below, there are 3 ways you can generate "rabbit" from s.
+rabbbit
+rabbbit
+rabbbit
+Example 2:
+Input: s = "babgbag", t = "bag"
+Output: 5
+Explanation:
+As shown below, there are 5 ways you can generate "bag" from s.
+babgbag
+babgbag
+babgbag
+babgbag
+babgbag
+
+Constraints:
+1 <= s.length, t.length <= 1000
+s and t consist of English letters.
+*/
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length(), n = t.length();
+        if (m < n) {
+            return 0;
+        }
+        vector<vector<unsigned long long>> dp(m + 1, vector<unsigned long long>(n + 1));
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            char sChar = s.at(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tChar = t.at(j);
+                if (sChar == tChar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
