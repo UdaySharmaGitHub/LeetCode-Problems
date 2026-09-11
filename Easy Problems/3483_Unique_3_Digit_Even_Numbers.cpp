26312411f87eda16c2c9c1c9eb897d956e47912d
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-09-11 23:49:38 +0530

    POTD 11-09-2026 : 3483 Unique 3 Digit Even Numbers [Easy]

---
+++ Easy Problems/3483_Unique_3_Digit_Even_Numbers.cpp
@@ -0,0 +1,67 @@
+/*
+3483. Unique 3-Digit Even Numbers
+You are given an array of digits called digits. Your task is to determine the number of distinct three-digit even numbers that can be formed using these digits.
+
+Note: Each copy of a digit can only be used once per number, and there may not be leading zeros.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3,4]
+
+Output: 12
+
+Explanation: The 12 distinct 3-digit even numbers that can be formed are 124, 132, 134, 142, 214, 234, 312, 314, 324, 342, 412, and 432. Note that 222 cannot be formed because there is only 1 copy of the digit 2.
+
+Example 2:
+
+Input: digits = [0,2,2]
+
+Output: 2
+
+Explanation: The only 3-digit even numbers that can be formed are 202 and 220. Note that the digit 2 can be used twice because it appears twice in the array.
+
+Example 3:
+
+Input: digits = [6,6,6]
+
+Output: 1
+
+Explanation: Only 666 can be formed.
+
+Example 4:
+
+Input: digits = [1,3,5]
+
+Output: 0
+
+Explanation: No even 3-digit numbers can be formed.
+
+ 
+
+Constraints:
+
+3 <= digits.length <= 10
+0 <= digits[i] <= 9
+*/
+class Solution {
+public:
+    int totalNumbers(auto& digits) {
+        int f[10] = {0};
+        int res = 0;
+
+        for (auto& d : digits)
+            f[d]++;
+
+        for (int i = 1; i < 10; i++) 
+            for (int j = 0; j < 10; j++) 
+                for (int k = 0; k < 9; k += 2) 
+                    res += f[i] > 0 &&
+                           f[j] > (i == j) &&
+                           f[k] > (i == k) + (j == k);
+        
+
+        return res;
+    }
+};
