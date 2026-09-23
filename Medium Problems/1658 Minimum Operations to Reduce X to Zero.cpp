658cf1b076afe591fb0981e4232f762648d302c7
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-09-23 23:44:38 +0530

    POTD 23-09-2026 : 1658 Minimum Operations to Reduce X to Zero [Medium]

---
+++ Medium Problems/1658 Minimum Operations to Reduce X to Zero.cpp
@@ -0,0 +1,41 @@
+/*
+1658. Minimum Operations to Reduce X to Zero
+You are given an integer array nums and an integer x. In one operation, you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x. Note that this modifies the array for future operations.
+Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise, return -1. 
+Example 1:
+Input: nums = [1,1,4,2,3], x = 5
+Output: 2
+Explanation: The optimal solution is to remove the last two elements to reduce x to zero.
+Example 2:
+Input: nums = [5,6,7,8,9], x = 4
+Output: -1
+Example 3:
+Input: nums = [3,2,20,1,1,3], x = 10
+Output: 5
+Explanation: The optimal solution is to remove the last three elements and the first two elements (5 operations in total) to reduce x to zero.
+
+Constraints:
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+1 <= x <= 109
+*/
+class Solution {
+public:
+    int minOperations(vector<int>& A, int x) {
+        int k = reduce(A.begin(), A.end()) - x, n = A.size();
+        if (k < 0) return -1;
+        if (k == 0) return n;
+
+        int best = -1, i = 0, sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum += A[j];
+            while (sum > k)
+                sum -= A[i++];
+
+            if (sum == k)
+                best = max(best, j - i + 1);
+        }
+
+        return best + 1 ? n - best : -1;
+    }
+};
